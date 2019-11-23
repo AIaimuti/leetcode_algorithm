168a6ae6d681ec41eee1e7a6d3a4132c334060b0
AIaimuti <545009377@qq.com>
2019-11-23 09:59:26 +0800

    Update leetcode-83- 删除排序链表中的重复元素.cpp

--- listnode/easy/leetcode-83- 删除排序链表中的重复元素.cpp
+++ listnode/easy/leetcode-83- 删除排序链表中的重复元素.cpp
@@ -1,11 +1,11 @@
-
+//方法1，辅助链表添加
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
         if (!head) return NULL;
         if (!head->next) return head;
-        ListNode dummy = ListNode(-1);  
-        ListNode* prev = &dummy;   
+        ListNode dummy = ListNode(-1);  //  创建栈空间对象即可。
+        ListNode* prev = &dummy;   // 注意这里需要取地址&
         while (head){
             while (head->next && head->val == head->next->val){
                 head = head->next;
@@ -15,5 +15,23 @@ public:
             head = head->next;
         }
         return dummy.next;
+    }
+};
+//方法2，直接修改
+
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        if (!head || !head->next) return head;
+        ListNode* prev = head;   // 注意这里需要取地址&
+        while (prev != NULL && prev->next != NULL){
+            if (prev->val == prev->next->val){
+                prev->next = prev->next->next;
+            }
+            else{
+                prev = prev->next;
+            }
+        }
+        return head;
     }
 };
